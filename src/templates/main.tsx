aaa026e8fab6c5ab20dd7fa10510ba515f033cca
nicenathapong <nicenathapong@gmail.com>
2023-07-10 18:40:57 +0700

    fix: Image broken

--- src/templates/main.tsx
+++ src/templates/main.tsx
@@ -2,7 +2,9 @@ import { ReactNode } from "react";
 
 export default function MainTemplate({ children }: { children: ReactNode }) {
     return (
-        <div className="min-h-screen bg-cover bg-center bg-[url(./images/bg-dark-blue-aura.png)] p-8 pb-36">
+        <div className="min-h-screen bg-cover bg-center p-8 pb-36" style={{
+            backgroundImage: "url(./images/bg-dark-blue-aura.png)"
+        }}>
             {children}
         </div>
     );
